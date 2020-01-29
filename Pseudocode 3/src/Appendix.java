9f9ba824cb54bb3080924e1c039b693480a0bf6d
Nick <knackepizza@gmail.com>
2020-01-28 23:22:23 -0500

    Added Appendix to PS3

---
+++ Pseudocode 3/src/Appendix.java
@@ -0,0 +1,11 @@
+public class Appendix {
+    public static void main(String[] args) {
+        String a = "CSE 1321";
+
+        if (a.equals("CSE 1321")) {
+            System.out.println("It is equal.");    
+        } else {
+            System.out.println("Not equal.");
+        }
+    }
+}
