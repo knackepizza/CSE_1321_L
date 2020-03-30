75b1aa3e086345388c9b4aa264c879f1b48bc201
Nick <knackepizza@gmail.com>
2020-03-29 20:37:28 -0400

    Updated Lab 12 and Assignment 6

--- Lab12/src/AssignGrades.java
+++ Lab12/src/AssignGrades.java
@@ -1,3 +1,11 @@
+/* Class: CSE 1321L
+ * Section: 10
+ * Term: Spring 2020
+ * Instructor: Imad Ghandour
+ * Name: Nick Nettleton
+ * Lab#: 12
+*/
+
 import java.util.Scanner;
 
 public class AssignGrades {
